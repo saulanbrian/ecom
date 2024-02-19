3cfe970cd9a3f0139e06f2cddd4751096040466e
saulanbrian <saulanbrian@gmail.com>
2024-02-19 12:07:21 +0800

    add fields to order model

--- order/models.py
+++ order/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+from products.models import Product
+
+class Order(models.Model):
+  buyer = models.ForeignKey(
+    User,
+    on_delete=models.CASCADE,
+    related_name='orders')
+  
+  product = models.ForeignKey(
+    Product,
+    on_delete=models.CASCADE,
+    related_name='orders')
+  
+  received = models.BooleanField(
+    default=False)
+  
+  def confirm_receive(self):
+    self.received = True
