49c273acc3020676be126871b13c0819597b5f06
saulanbrian <saulanbrian@gmail.com>
2024-02-19 12:39:30 +0800

    major fixes

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order
+
+class OrderAdmin(admin.ModelAdmin):
+  class Meta:
+    field = '__all__'
+
+admin.site.register(Order,OrderAdmin)
