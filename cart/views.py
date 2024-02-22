b64152f75fa9c3d97823286e203cdcf16adc96f4
saulanbrian <saulanbrian@gmail.com>
2024-02-22 22:57:22 +0800

    modify cart app

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+from .models import Cart
+
+from django.urls import reverse_lazy
+
+@login_required(
+  login_url=reverse_lazy('login'))
+def cart(request):
+  user_id = request.user.id
+  products = Cart.objects.get(
+    user__id=user_id).products.all()
+  return render(
+    request,
+    'cart/index.html',
+    {'products':products})
+ 
+  
--- ecom/urls.py
+++ ecom/urls.py
@@ -32,7 +32,11 @@ urlpatterns = [
     path(
       'products/',
       include('products.urls')
-    )
+    ),
+    path(
+      'cart/',
+      include('cart.urls')
+      )
 ] + static(
   settings.MEDIA_URL,
   document_root=settings.MEDIA_ROOT
